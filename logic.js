0a872bfee6aa21c6fec043d94009f46edbaa0480
Stephen Howard <Showard1793@gmail.com>
2024-08-05 15:14:02 -0500

    1.1

--- logic.js
+++
@@ -1,8 +0,0 @@
-let arr = [1,2,5,4,3];
-let arr2 = ["cat", "dog"];
-
-console.log(arr.sort());
-
-//git remote add origin https://github.com/Showard1793/testProject.git
-//git branch -M main
-//git push -u origin main
---
+++ script.js
@@ -0,0 +1,14 @@
+function youRang() {
+    document.getElementById("main-text").innerHTML += " You rang...";
+    console.log("you rang...");
+}
+
+function erase() {
+    document.getElementById("main-text").innerHTML = "Ring the doorbell...if you dare!";
+    console.log("you rang..."); 
+}
+
+
+//git remote add origin https://github.com/Showard1793/testProject.git
+//git branch -M main
+//git push -u origin main
