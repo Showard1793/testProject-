b43b84d41cf4755eaf17b3089d7dc646c0cb4314
Stephen Howard <Showard1793@gmail.com>
2024-08-08 12:46:38 -0500

    Update

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-function youRang() {
-    document.getElementById("main-text").innerHTML += " You rang...";
-    console.log("you rang...");
-}
+window.addEventListener("load", () => {
 
-function erase() {
-    document.getElementById("main-text").innerHTML = "Ring the doorbell...if you dare!";
-    console.log("you rang..."); 
-}
+//CONVERT HTML ELEMENTS TO JAVASCRIPT VARIABLES
+const mainText = document.getElementById("main-text")
+const doorBell = document.getElementById("doorbell-button")
+const erase = document.getElementById("erase-button")
+
+//ADD FUNCTIONS FOR EVENTS
+doorBell.addEventListener("click", ()=>{
+    mainText.innerHTML += " you rang..."
+})
+
+erase.addEventListener("click", ()=>{
+    mainText.innerHTML = "Ring the doorbell...if you dare!"
+})
+
+erase.addEventListener("mouseover", ()=>{
+    erase.style.backgroundColor = "red"
+})
+
+erase.addEventListener("mouseleave", ()=>{
+    erase.style.backgroundColor = ""
+})
+
+});
+
+
+
+
+
+
+
 
 
 //git remote add origin https://github.com/Showard1793/testProject.git
